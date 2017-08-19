da21be78afe1de365f37ed9f36c788a08c7b2940
Pham <cl93821@163.com>
2017-08-19 12:41:27 +0800

    version 1.0:正式版，使用fabric进行部署

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='vocabulary_book',
+      version='1.0',
+      description='vocabulary book server',
+      author='Pham',
+      license='MIT',
+      packages=find_packages()
+      )
